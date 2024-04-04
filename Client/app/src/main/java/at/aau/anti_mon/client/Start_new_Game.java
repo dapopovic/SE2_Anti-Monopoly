70260ea34df0f12521b51b64e7b9ffb2023029e7
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-04-04 16:20:21 +0200

    Create Start new Game

--- Client/app/src/main/java/at/aau/anti_mon/client/Start_new_Game.java
+++ Client/app/src/main/java/at/aau/anti_mon/client/Start_new_Game.java
@@ -1,9 +1,7 @@
 package at.aau.anti_mon.client;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Start_Page extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class Start_new_Game extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start_page);
+        setContentView(R.layout.activity_start_new_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        Button StartnewGame = findViewById(R.id.start_new_game);
-        StartnewGame.setOnClickListener(
-                v ->{
-                    Intent intent = new Intent(Start_Page.this, Start_new_Game.class);
-                    startActivity(intent);
-        });
     }
 }
