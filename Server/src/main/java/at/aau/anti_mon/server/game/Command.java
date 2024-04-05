acadb8b59c505aa811baf79d2ce56070942dcc17
Anazorex <anazorex.bus@gmail.com>
2024-04-05 18:33:39 +0200

    Refactor Command class to use lombok annotations for constructor

--- Server/src/main/java/at/aau/anti_mon/server/game/Command.java
+++ Server/src/main/java/at/aau/anti_mon/server/game/Command.java
@@ -1,15 +1,12 @@
 package at.aau.anti_mon.server.game;
 
 import com.google.gson.JsonObject;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Command {
     private final Commands command;
     private final JsonObject data;
-
-    public Command(Commands command, JsonObject data) {
-        this.command = command;
-        this.data = data;
-    }
 }
