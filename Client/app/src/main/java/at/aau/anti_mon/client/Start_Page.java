488e39f039e5ea288447b6a1ee197a479bae3273
JulieL999 <julie.lomonosova283@gmail.com>
2024-04-07 18:54:24 +0200

    Created design for the join game page

--- Client/app/src/main/java/at/aau/anti_mon/client/Start_Page.java
+++ Client/app/src/main/java/at/aau/anti_mon/client/Start_Page.java
@@ -1,7 +1,9 @@
 package at.aau.anti_mon.client;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +17,18 @@ public class Start_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start_page);
+        setContentView(R.layout.activity_start_page_landscape);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button joinGameBtn = findViewById(R.id.join_game);
+        joinGameBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Join_Game.class);
+            startActivity(intent);
+        });
     }
 }
