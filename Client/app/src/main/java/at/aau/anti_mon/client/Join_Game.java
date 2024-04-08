6b0deeb638d40155df9fdbc1f24072a4bb464d34
JulieL999 <julie.lomonosova283@gmail.com>
2024-04-08 17:57:10 +0200

    Created design for the join game page, I am trying to correct an error and put the class that was not pushed

---
+++ Client/app/src/main/java/at/aau/anti_mon/client/Join_Game.java
@@ -0,0 +1,35 @@
+package at.aau.anti_mon.client;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Join_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_join_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Start_Page.class);
+            startActivity(intent);
+        });
+    }
+}
