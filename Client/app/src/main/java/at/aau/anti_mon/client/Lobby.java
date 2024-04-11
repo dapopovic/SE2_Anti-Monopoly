058b6d220b9bdcf134bb60c851a635750902aff6
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-04-11 17:19:04 +0200

    General UI
    The General UI for the game

--- Client/app/src/main/java/at/aau/anti_mon/client/Lobby.java
+++ Client/app/src/main/java/at/aau/anti_mon/client/Lobby.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -38,6 +37,11 @@ public class Lobby extends AppCompatActivity {
 
     public void onCancelLobby(View view) {
         Intent intent = new Intent(Lobby.this, Start_new_Game.class);
+        startActivity(intent);
+    }
+
+    public void onStartGame(View view) {
+        Intent intent = new Intent(Lobby.this, activityGamefield.class);
         startActivity(intent);
     }
 }
