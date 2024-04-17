0e6ef3f0be6b12187466ed98846cb04626f8e2e3
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-04-17 14:55:08 +0200

    UI
    The UI for Load Game and Game instructions

--- Client/app/src/main/java/at/aau/anti_mon/client/GameInstructions.java
+++ Client/app/src/main/java/at/aau/anti_mon/client/GameInstructions.java
@@ -1,45 +1,31 @@
 package at.aau.anti_mon.client;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import at.aau.anti_mon.client.networking.WebSocketClient;
 
-public class Start_Page extends AppCompatActivity {
+public class GameInstructions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start_page);
+        setContentView(R.layout.activity_game_instructions);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void Start_new_game(View view) {
-        Intent intent = new Intent(Start_Page.this, Start_new_Game.class);
-        startActivity(intent);
-    }
 
-    public void LoadGame(View view) {
-        Intent intent = new Intent(Start_Page.this, LoadGame.class);
-        startActivity(intent);
-    }
-
-    public void GameInstructions(View view) {
-        Intent intent = new Intent(Start_Page.this, GameInstructions.class);
+    public void onCancelGameInstructions(View view) {
+        Intent intent = new Intent(GameInstructions.this, Start_Page.class);
         startActivity(intent);
     }
 }
-
