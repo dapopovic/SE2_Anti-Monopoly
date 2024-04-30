f53e954c2a84f267c70984a3a71b1fc3e0b3e156
Anazorex <anazorex.bus@gmail.com>
2024-04-30 20:34:38 +0200

    Add unit test for pinCommand and changed build.gradle

--- Client/app/src/test/java/at/aau/anti_mon/client/unittests/CommandsTest.java
+++ Client/app/src/test/java/at/aau/anti_mon/client/unittests/CommandsTest.java
@@ -1,7 +1,10 @@
 package at.aau.anti_mon.client.unittests;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+
+import android.util.Log;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +13,12 @@ import org.mockito.MockitoAnnotations;
 
 import at.aau.anti_mon.client.command.Commands;
 import at.aau.anti_mon.client.command.JoinGameCommand;
+import at.aau.anti_mon.client.command.LeaveGameCommand;
+import at.aau.anti_mon.client.command.PinCommand;
 import at.aau.anti_mon.client.events.GlobalEventQueue;
+import at.aau.anti_mon.client.events.PinReceivedEvent;
 import at.aau.anti_mon.client.events.UserJoinedLobbyEvent;
+import at.aau.anti_mon.client.events.UserLeftLobbyEvent;
 import at.aau.anti_mon.client.json.JsonDataDTO;
 import at.aau.anti_mon.client.viewmodels.LobbyViewModel;
 
@@ -25,8 +32,8 @@ class CommandsTest {
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-
     }
+    
     @Test
     void joinGameCommandShouldFireUserJoinedLobbyEvent() {
         JsonDataDTO jsonDataDTO = new JsonDataDTO();
@@ -40,4 +47,29 @@ class CommandsTest {
         verify(queue).enqueueEvent(any(UserJoinedLobbyEvent.class));
         verify(viewModel).userJoined("testUser");
     }
+    @Test
+    void leaveGameCommandShouldFireUserLeftLobbyEvent() {
+        JsonDataDTO jsonDataDTO = new JsonDataDTO();
+        jsonDataDTO.setCommand(Commands.LEAVE_GAME);
+        jsonDataDTO.putData("username", "testUser");
+
+        LeaveGameCommand leaveGameCommand = new LeaveGameCommand(queue, viewModel);
+        leaveGameCommand.execute(jsonDataDTO);
+
+        verify(queue).enqueueEvent(any(UserLeftLobbyEvent.class));
+        verify(viewModel).userLeft("testUser");
+    }
+
+    @Test
+    void pinCommandShouldFirePinReceivedEvent() {
+        JsonDataDTO jsonDataDTO = new JsonDataDTO();
+        jsonDataDTO.setCommand(Commands.PIN);
+        jsonDataDTO.putData("pin", "1234");
+
+        PinCommand pinCommand = new PinCommand(queue);
+        pinCommand.execute(jsonDataDTO);
+
+        verify(queue).enqueueEvent(any(PinReceivedEvent.class));
+    }
+
 }
