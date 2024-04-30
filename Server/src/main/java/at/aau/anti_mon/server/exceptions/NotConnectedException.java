694c197582f5047f2f1e78f3a9b4a4c859c1d668
Anazorex <anazorex.bus@gmail.com>
2024-04-30 19:06:46 +0200

    Remove unused exception classes

--- Server/src/main/java/at/aau/anti_mon/server/exceptions/NotConnectedException.java
+++
@@ -1,7 +0,0 @@
-package at.aau.anti_mon.server.exceptions;
-
-public class NotConnectedException extends Exception {
-    public NotConnectedException() {
-        super("Spieler ist nicht mehr verbunden!");
-    }
-}
--- Server/src/main/java/at/aau/anti_mon/server/exceptions/PlayerAlreadyInLobbyException.java
+++
@@ -1,7 +0,0 @@
-package at.aau.anti_mon.server.exceptions;
-
-public class PlayerAlreadyInLobbyException extends Exception {
-    public PlayerAlreadyInLobbyException() {
-        super("Spieler ist bereits in der Lobby!");
-    }
-}
