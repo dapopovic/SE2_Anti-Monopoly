b6506430abfcef891d923f8e94dba8712646ab2e
Anazorex <anazorex.bus@gmail.com>
2024-05-01 09:39:45 +0200

    setup Integration testing for Android Websocket

--- Client/app/src/test/java/at/aau/anti_mon/client/integrationtests/WebSocketClientTest.java
+++ Client/app/src/test/java/at/aau/anti_mon/client/integrationtests/WebSocketClientTest.java
@@ -1,58 +1,60 @@
 package at.aau.anti_mon.client.integrationtests;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import javax.inject.Inject;
 
 import at.aau.anti_mon.client.AntiMonopolyApplication;
-import at.aau.anti_mon.client.AppComponent;
+import at.aau.anti_mon.client.command.Commands;
 import at.aau.anti_mon.client.events.GlobalEventQueue;
+import at.aau.anti_mon.client.events.PinReceivedEvent;
+import at.aau.anti_mon.client.json.JsonDataDTO;
+import at.aau.anti_mon.client.json.JsonDataManager;
 import at.aau.anti_mon.client.networking.NetworkModule;
 import at.aau.anti_mon.client.networking.WebSocketClient;
-import at.aau.anti_mon.client.websocketserver.WebsocketHandlerServerImpl;
 
 public class WebSocketClientTest extends AntiMonopolyApplication {
-    private static WebsocketHandlerServerImpl server;
+
     @Inject
     WebSocketClient client;
-    @BeforeAll
-    static void setUp() {
-        server = new WebsocketHandlerServerImpl();
-        try {
-            server.start();
-        } catch (Exception e) {
-            fail("Failed to start server");
-        }
-
-    }
+    @Mock
+    GlobalEventQueue globalEventQueue;
 
     @BeforeEach
     void init() {
+        openMocks(this);
         TestComponent testComponent = DaggerTestComponent.builder().networkModule(new NetworkModule(this)).build();
-        GlobalEventQueue globalEventQueue = new GlobalEventQueue();
-        globalEventQueue.setEventBusReady(true);
         setGlobalEventQueue(globalEventQueue);
         testComponent.inject(this);
     }
 
-    @AfterAll
-    static void tearDown() {
-        try {
-            server.stop();
-        } catch (Exception e) {
-            fail("Failed to stop server");
-        }
-    }
-
     @Test
     void testNewCreateGameCommandAndGetPin() {
-        String uri = "ws://localhost:8080";
-        client.connectToServer(uri);
+        client.setUserId("test");
+        client.connectToServer();
+        assertTrue(client.isConnected());
+
+        JsonDataDTO jsonDataDTO = new JsonDataDTO();
+        jsonDataDTO.setCommand(Commands.CREATE_GAME);
+        jsonDataDTO.putData("username", "test");
+        String message = JsonDataManager.createJsonMessage(jsonDataDTO);
+
+        client.sendMessageToServer(message);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            fail("Thread interrupted");
+        }
+        verify(globalEventQueue).enqueueEvent(any(PinReceivedEvent.class));
+
     }
 
 }
