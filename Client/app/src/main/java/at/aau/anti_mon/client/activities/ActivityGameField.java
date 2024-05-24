d2932410e82d5c714e7b2e372973b7510e575e2f
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-05-24 14:21:03 +0200

    change diceButton
    make it not work after use

--- Client/app/src/main/java/at/aau/anti_mon/client/activities/ActivityGameField.java
+++ Client/app/src/main/java/at/aau/anti_mon/client/activities/ActivityGameField.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -146,6 +148,9 @@ public class ActivityGameField extends AppCompatActivity {
     }
 
     public void onFigureMove(View view) {
+        ImageButton b = findViewById(R.id.btnDice);
+        b.setEnabled(false);
+
         int randomNumber = random.nextInt(11) + 2;
         String dice = String.valueOf(randomNumber);
         String user = currentUser.getUsername();
