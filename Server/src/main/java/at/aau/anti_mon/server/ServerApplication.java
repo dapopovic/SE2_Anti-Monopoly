82bc65bdbca306351aa93965dffb9e8e60f05eda
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-06-15 16:35:40 +0200

    remove imports

--- Server/src/main/java/at/aau/anti_mon/server/ServerApplication.java
+++ Server/src/main/java/at/aau/anti_mon/server/ServerApplication.java
@@ -1,11 +1,7 @@
 package at.aau.anti_mon.server;
 
-import at.aau.anti_mon.server.websocket.configuration.SchedulingConfig;
-import at.aau.anti_mon.server.websocket.configuration.WebSocketBrokerConfig;
-import at.aau.anti_mon.server.websocket.configuration.WebSocketHandlerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 //@EnableConfigurationProperties({SchedulingConfig.class, WebSocketHandlerConfig.class, WebSocketBrokerConfig.class })
--- Server/src/main/java/at/aau/anti_mon/server/commands/FirstPlayerCommand.java
+++ Server/src/main/java/at/aau/anti_mon/server/commands/FirstPlayerCommand.java
@@ -2,7 +2,6 @@ package at.aau.anti_mon.server.commands;
 
 import at.aau.anti_mon.server.dtos.JsonDataDTO;
 import at.aau.anti_mon.server.events.FirstPlayerEvent;
-import at.aau.anti_mon.server.events.NextPlayerEvent;
 import at.aau.anti_mon.server.exceptions.CanNotExecuteJsonCommandException;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.socket.WebSocketSession;
